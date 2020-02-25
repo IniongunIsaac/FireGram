ad806a0ab27fa120d2170b835cb4fcd458cf87a2
Isaac Iniongun <iniongunisaac@gmail.com>
2020-02-25 10:25:48 +0100

    Add Designs for login and sign up screens; Setup tab controller with empty views.

---
+++ FireGram/DiscoverViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DiscoverViewController.swift
+//  FireGram
+//
+//  Created by Isaac Iniongun on 25/02/2020.
+//  Copyright © 2020 Iniongun Group. All rights reserved.
+//
+
+import UIKit
+
+class DiscoverViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
